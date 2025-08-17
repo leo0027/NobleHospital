cb8b011de40586f5085918d8bfb7d09339dcc787
Saksham Maruti Pilane <Sakshampilane.04@gmail.com>
2025-08-17 19:03:01 +0530

    Fix MySQL connection for Render with Railway public host

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 # -------------------- DATABASE --------------------
 mydb = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT", 3306))
+    host=os.environ.get("MYSQLHOST", "caboose.proxy.rlwy.net"),
+    port=os.environ.get("MYSQLPORT", 58152),
+    user=os.environ.get("MYSQLUSER", "root"),
+    password=os.environ.get("MYSQLPASSWORD", "BIcTYywpKcTyvsJtONXHlHKhklhCYFgC"),
+    database=os.environ.get("MYSQLDATABASE", "railway")
 )
 mycursor = mydb.cursor()
 
